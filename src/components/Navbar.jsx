ab1995c39e1c7330da6b49857d2f85aadb7fe165
Robert Maina <robert-devs>
2023-07-22 21:54:00 +0300

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); 
   }, []);
 
   return (
